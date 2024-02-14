1ec4188421f4bd2ba64a02519ed7d6d8723af5df
Kabindra Shrestha <kabindra@vianet.com.np>
2024-02-14 17:55:16 +0545

    Worked on carousal inside a customscrollview

--- lib/presentation/pages/home/home_page.dart
+++ lib/presentation/pages/home/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:sample_clean_architecture/core/extensions/context_extension.dart';
@@ -25,7 +26,8 @@ class HomePage extends StatelessWidget {
             return state.when(
                 initial: () => const SizedBox.shrink(),
                 loading: () => const PlatformLoadingIndicatorWidget(),
-                success: (List<Article> articles) => _buildArticles(articles),
+                success: (List<Article> articles) =>
+                    _buildArticles(context, articles),
                 error: (String error) => Container(
                       alignment: Alignment.center,
                       color: Colors.lightBlue[100 * (10 % 9)],
@@ -49,7 +51,8 @@ class HomePage extends StatelessWidget {
             return state.when(
                 initial: () => const SizedBox.shrink(),
                 loading: () => const PlatformLoadingIndicatorWidget(),
-                success: (List<Article> articles) => _buildArticles(articles),
+                success: (List<Article> articles) =>
+                    _buildArticles(context, articles),
                 error: (String error) => Container(
                       alignment: Alignment.center,
                       color: Colors.lightBlue[100 * (10 % 9)],
@@ -69,6 +72,7 @@ class HomePage extends StatelessWidget {
   }
 
   Widget _buildArticles(
+    BuildContext context,
     List<Article> articles,
   ) {
     return CustomScrollView(
@@ -77,32 +81,88 @@ class HomePage extends StatelessWidget {
           floating: true,
           pinned: true,
           expandedHeight: 250.0,
-          flexibleSpace: FlexibleSpaceBar(
-            title: Text(
-              'Articles',
-              maxLines: 1,
-              overflow: TextOverflow.ellipsis,
+          flexibleSpace: SafeArea(
+            child: FlexibleSpaceBar(
+              title: Text(
+                'Articles',
+                maxLines: 1,
+                overflow: TextOverflow.ellipsis,
+              ),
             ),
           ),
           backgroundColor: Colors.blueAccent,
         ),
+        SliverToBoxAdapter(
+          child: CarouselSlider(
+            options: CarouselOptions(
+                height: 180.0,
+                autoPlay: true,
+                aspectRatio: 2.0,
+                enlargeCenterPage: true),
+            items: articles
+                .sublist(0, 5)
+                .map((item) => Container(
+                      margin: const EdgeInsets.all(5.0),
+                      child: ClipRRect(
+                          borderRadius:
+                              const BorderRadius.all(Radius.circular(5.0)),
+                          child: Stack(
+                            children: <Widget>[
+                              Image.network(item.urlToImage!,
+                                  fit: BoxFit.cover, width: 1000.0),
+                              Positioned(
+                                bottom: 0.0,
+                                left: 0.0,
+                                right: 0.0,
+                                child: Container(
+                                  decoration: const BoxDecoration(
+                                    gradient: LinearGradient(
+                                      colors: [
+                                        Color.fromARGB(200, 0, 0, 0),
+                                        Color.fromARGB(0, 0, 0, 0)
+                                      ],
+                                      begin: Alignment.bottomCenter,
+                                      end: Alignment.topCenter,
+                                    ),
+                                  ),
+                                  padding: const EdgeInsets.symmetric(
+                                      vertical: 10.0, horizontal: 20.0),
+                                  child: Text(
+                                    '${item.title}',
+                                    maxLines: 3,
+                                    overflow: TextOverflow.ellipsis,
+                                    style: context.subTitle1Context,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          )),
+                    ))
+                .toList(),
+          ),
+        ),
         SliverGrid(
-          gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
-            maxCrossAxisExtent: 200.0,
-            mainAxisSpacing: 10.0,
-            crossAxisSpacing: 10.0,
-            childAspectRatio: 4.0,
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 3,
+            mainAxisSpacing: 4.0,
+            crossAxisSpacing: 4.0,
+            childAspectRatio: 1.0,
           ),
           delegate: SliverChildBuilderDelegate(
             (BuildContext context, int index) {
-              return Container(
-                alignment: Alignment.center,
-                color: Colors.teal[100 * (index % 9)],
-                child: Text(
-                  articles[index].title!,
-                  maxLines: 1,
-                  overflow: TextOverflow.ellipsis,
-                  style: context.subTitle1Context,
+              return GestureDetector(
+                onTap: () {
+                  print('Item $index clicked');
+                },
+                child: Container(
+                  alignment: Alignment.center,
+                  color: Colors.teal[100 * (index % 9)],
+                  child: Text(
+                    articles[index].title!,
+                    maxLines: 1,
+                    overflow: TextOverflow.ellipsis,
+                    style: context.subTitle1Context,
+                  ),
                 ),
               );
             },
@@ -113,14 +173,19 @@ class HomePage extends StatelessWidget {
           itemExtent: 50.0,
           delegate: SliverChildBuilderDelegate(
             (BuildContext context, int index) {
-              return Container(
-                alignment: Alignment.center,
-                color: Colors.lightBlue[100 * (index % 9)],
-                child: Text(
-                  articles[index].title!,
-                  maxLines: 1,
-                  overflow: TextOverflow.ellipsis,
-                  style: context.subTitle1Context,
+              return GestureDetector(
+                onTap: () {
+                  print('Item $index clicked');
+                },
+                child: Container(
+                  alignment: Alignment.center,
+                  color: Colors.lightBlue[100 * (index % 9)],
+                  child: Text(
+                    articles[index].title!,
+                    maxLines: 1,
+                    overflow: TextOverflow.ellipsis,
+                    style: context.subTitle1Context,
+                  ),
                 ),
               );
             },
